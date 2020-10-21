bf0c5e3f8ef9d95ce24daba2fe7d589d10c34579
Jakob Persons <jakobep24@gmail.com>
2020-10-21 15:56:33 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -18,5 +18,5 @@ daily_temps = [
 def find_min_in_nested_arrays(src)
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
-  
+
 end
